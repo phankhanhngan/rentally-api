4b4a686058980d7587c4e1903d1b430b24d73e43
NoIr <vokhuong@gmail.com>
2023-09-30 11:25:46 +0700

    delete .env

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -126,7 +126,7 @@ export class UsersService {
       filterMessageDto.sortField.forEach((field) => {
         if (!fields.includes(field))
           throw new BadRequestException(
-            'sortField must be one of id, googleId, email, firstName, role, phoneNumber',
+            'sortField must be one of id, googleId, email, firstName, lastName, role, phoneNumber',
           );
       });
 
