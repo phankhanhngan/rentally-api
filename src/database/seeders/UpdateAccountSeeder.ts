bf8973c64238be12a97318b95c845f425b030570
nganphan <phankhanhnganngan@gmail.com>
2023-12-06 12:40:14 +0700

    Seed stripe account

--- src/database/seeders/UpdateAccountSeeder.ts
+++ src/database/seeders/UpdateAccountSeeder.ts
@@ -1,14 +1,21 @@
 import { Seeder } from '@mikro-orm/seeder';
 import { EntityManager } from '@mikro-orm/core';
 import { vietnamePhone } from './jsData/vietnamePhone';
+import { account } from './jsData/stripeConnectedAccount';
 import { User } from '../../entities';
+import { Role } from '../../common/enum/common.enum';
 
 export class UpdateAccountSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    const users = await em.find<User>(User, {});
+    const users = await em.find<User>(User, { role: Role.MOD }, { limit: 12 });
     const phoneNumbers = vietnamePhone;
+    const stripeAccount = account;
     users.map((el, i) => {
-      el.phoneNumber = phoneNumbers[i].number;
+      // el.phoneNumber = phoneNumbers[i].number;
+      el.bankCode = '01101100';
+      el.accountNumber = '000123456789';
+      el.stripeAccountId = account[i].account;
+      el.stripeBankAccountId = account[i].bank;
     });
     await em.persistAndFlush(users);
   }
---
+++ src/database/seeders/jsData/stripeConnectedAccount.ts
@@ -0,0 +1,52 @@
+export const account = [
+  {
+    account: 'acct_1OKDCkQ3pGsssaYF',
+    bank: 'ba_1OKDCmQ3pGsssaYFCrBAz3Sq',
+  },
+  {
+    account: 'acct_1OKBioQ57BA0GDJK',
+    bank: 'ba_1OKBiqQ57BA0GDJKq0X0NBxd',
+  },
+  {
+    account: 'acct_1OKBeoPwavBzdZU0',
+    bank: 'ba_1OKBeqPwavBzdZU0hEsUj8LR',
+  },
+  {
+    account: 'acct_1OKBd7Q8I6anN7Hl',
+    bank: 'ba_1OKBd9Q8I6anN7HlDhVWkHOc',
+  },
+  {
+    account: 'acct_1OJpV6Q5wG05Ub80',
+    bank: 'ba_1OJpV8Q5wG05Ub80FV5LxXAh',
+  },
+  {
+    account: 'acct_1OJpT1Q7HgC2LPUi',
+    bank: 'ba_1OJpT9Q7HgC2LPUiPbL9weNh',
+  },
+  {
+    account: 'acct_1OJpPxPpOIFiBNsg',
+    bank: 'ba_1OJpQ0PpOIFiBNsgowMnAdWi',
+  },
+  {
+    account: 'acct_1OJHUgQ35P7Kp3v1',
+    bank: 'ba_1OJHUiQ35P7Kp3v1ovgHLKKU',
+  },
+  {
+    account: 'acct_1OJH44Q0HA30TJEn',
+    bank: 'ba_1OJH47Q0HA30TJEnylp7nxxf',
+  },
+  {
+    account: 'acct_1OJGexQ4zXXCagIK',
+    bank: 'ba_1OJGf0Q4zXXCagIKZ6n1cawn',
+  },
+  {
+    account: 'acct_1OJDBHQ6IG6jntA0',
+    bank: 'ba_1OJDBKQ6IG6jntA0TDd9gLsc',
+  },
+  {
+    account: 'acct_1OJD05Q2Aov9f9Sg ',
+    bank: 'ba_1OJD08Q2Aov9f9SgclmmKePY',
+  },
+];
+
+console.log(account.length)
